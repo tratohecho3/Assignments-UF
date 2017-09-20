cd5401854429ae9369c024f91d24345434bc3518
César Colina <tratohecho3@gmail.com>
2017-09-20 15:44:16 -0400

    Add files via upload

---
+++ server/config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  db: {
+    uri: 'mongodb://ayacayu:DanielC123@ds129144.mlab.com:29144/pruebadb', //place the URI of your mongo database here.
+  }, 
+  googleMaps: {
+    key: ''
+  },
+  port: 8080
+};
