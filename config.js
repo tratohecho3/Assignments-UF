d0c2ec6addc3c175797d23190fc6a4bc8422b843
César Colina <tratohecho3@gmail.com>
2017-09-23 14:56:15 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   db: {
-    uri: 'mongodb://ayacayu:DanielC123@ds129144.mlab.com:29144/pruebadb', //place the URI of your mongo database here.
+    uri: '', //place the URI of your mongo database here.
   }
 };
